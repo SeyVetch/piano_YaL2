7913e28525f688fb054c549b296ea6219b2f411a
Александр Маханов <dendiherobrine@gmail.com>
2018-12-08 19:40:19 +0300

    third draft

--- test.py
+++ test.py
@@ -7,16 +7,24 @@ pygame.midi.init()
 player = pygame.midi.Output(0) 
 player.set_instrument(1) 
 
+octave = 1
+flag = dict()
+for i in 'qwertyuiop[]':
+    flag[i] = False
+note = 'qwertyuiop[]'
+octaves = 'zxcvbnm'
+
 while True:
-    try:
-        if keyboard.is_pressed('q'):#if key 'q' is pressed 
-            z=random.randint(0, 64) 
-            player.note_on(z, 127) 
-            time.sleep(0.3) 
-            player.note_off(z, 127) 
-            time.sleep(0.1) 
-        elif keyboard.is_pressed('w'):
-            z=random.randint(64, 108)
+    try: 
+        for i in range(7):
+            if keyboard.is_pressed(octaves[i]):
+                octave = i + 1
+        for i in range(12):
+            if keyboard.is_pressed(note[i]) and not flag[note[i]]:
+                player.note_on(octave*12 + 24 + i, 127) 
+                flag[note[i]] = True
+        if keyboard.is_pressed('s'):
+            z=random.randint(24, 106)
             player.note_on(z, 127) 
             time.sleep(0.3) 
             player.note_off(z, 127) 
@@ -25,6 +33,10 @@ while True:
             break
         else:
             pass
+        for i in range(12):
+            if not keyboard.is_pressed(note[i]):
+                player.note_off(octave*12 + 24 + i, 127) 
+                flag[note[i]] = False
     except:
         pass 
         
